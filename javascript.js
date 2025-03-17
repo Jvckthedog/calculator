3ca1c895aebf9d783b0228564ad6736e2b7d61cc
jvckthedog <jmccollum26@gmail.com>
2025-03-17 11:33:54 -0700

    Update JS - 3.17

---
+++ javascript.js
@@ -0,0 +1,37 @@
+// Query Selectors 
+const display = document.querySelector('.display');
+const buttonOne = document.querySelector('.one');
+const buttonTwo = document.querySelector('.two');
+const buttonThree = document.querySelector('.three');
+const buttonFour = document.querySelector('.four');
+const buttonFive = document.querySelector('.five');
+const buttonSix = document.querySelector('.six');
+const buttonSeven = document.querySelector('.seven');
+const buttonEight = document.querySelector('.eight');
+const buttonNine = document.querySelector('.nine');
+const buttonTen = document.querySelector('.ten');
+const buttonAdd = document.querySelector('.add');
+const buttonSubstract = document.querySelector('.subtract');
+const buttonMultiply = document.querySelector('.multiply');
+const buttonDivide = document.querySelector('.divide');
+const buttonClear = document.querySelector('.clear');
+const buttonDelete = document.querySelector('.delete');
+const buttonZero = document.querySelector('.zero');
+const buttonDot = document.querySelector('.dot');
+const buttonEqual = document.querySelector('.equal');
+
+
+// Event Listeners // 
+
+// display output 
+const displayOutput = document.createElement('input');
+displayOutput.addEventListener('input', )
+
+buttonOne.addEventListener('click', () =>  {
+    const numberOne = 1;
+    console.log(numberOne); 
+}); 
+
+
+
+
